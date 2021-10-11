0ccabcf802bac969e071b3b5404544663ac95055
LACourtenay <ladc1995@gmail.com>
2021-10-11 10:22:43 +0200

    Update load_marks.R

--- R/load_marks.R
+++ R/load_marks.R
@@ -13,10 +13,10 @@
 #'
 #' @param file_name A string containing the path of the Ikhnos product file.
 #' If no file name is specified, then a popup window will appear allowing the user
-#' to locat the file in their system.
+#' to locate the file in their system.
 #' @param mark_type A lower case string specifying whether the marks under study
 #' are "circular" or "linear".
-#' @param plot A TRUE or FALSE option defining whether to produce a 3D plot of the marks.
+#' @param plot A boolean TRUE or FALSE option defining whether to produce a 3D plot of the marks.
 #' @param delim A string specifying how marks are delimited within the Ikhnos product file.
 #' @param colour_value A lower case string specifying the colour of the marks in the case where
 #' plot = TRUE.
@@ -45,30 +45,30 @@ load_marks <- function(file_name = NULL, mark_type = "circular",
 
 
   if (mark_type %!in% possible_marks) {
-    return(warning("Please select between circular or linear mark types"))
+    return(stop("Please select between circular or linear mark types"))
   }
 
   if (!is.logical(plot)) {
-    return(warning("plot parameter must be equal to TRUE or FALSE"))
+    return(stop("plot parameter must be equal to TRUE or FALSE"))
   }
 
   possible_delims <- c(",", ";", "\t", " ")
 
   if (delim %!in% possible_delims) {
-    return(warning(cat("Invalid delimiter value. Please ensure files are seperated",
-                       "by either commas, semi colons, tabs or spaces.",
-                       sep = " ")))
+    return(stop(cat("Invalid delimiter value. Please ensure files are seperated",
+                    "by either commas, semi colons, tabs or spaces.",
+                    sep = " ")))
   }
 
   if (colour_value != "black") {
     col_bool <- check_colours(colour_value)
     if (FALSE %in% col_bool) {
-      return(warning("Invalid colour provided for the plot"))
+      return(stop("Invalid colour provided for the plot"))
     }
   }
 
   if (!is.numeric(mark_size) | mark_size < 0) {
-    return(warning("point_size must be a positive value"))
+    return(stop("point_size must be a positive value"))
   }
 
   if (!is.null(file_name)) {
