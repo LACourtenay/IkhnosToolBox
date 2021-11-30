58d0f53f3edd1f39b24d96f2ce94ead4b00dcda3
JuliaAramendi <julia.aramendi@gmail.com>
2021-11-30 14:19:08 +0100

    prueba

---
+++ R/IkhnosToolBox.support.code.r
@@ -0,0 +1,283 @@
+#' @name IkhnosToolBox-package
+#' @docType package
+#' @aliases IkhnosToolBox
+#' @title R Tool Box for the Ikhnos Taphonomic Software
+#' @author Lloyd A. Courtenay and Julia Aramendi
+#' @description Group of R functions for the statistical analysis of data derived from the
+#' Ikhnos taphonomic software. Functions in this package allow one to visualise the
+#' location and orientation of bone surface modifications such as tooth and percussion pits,
+#' cut marks and tooth scores on long bones and their analysis for the identification and
+#' comparison of orientation and distribution patterns along the bone surface. Functions
+#' are available to perform analyses within samples as well as across different samples.
+NULL
+
+#' x, y and z coordinates for pits on right large-sized ungulate femora
+#'
+#' @name femur_right_circular1
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right medium-sized ungulate femora
+#'
+#' @name femur_right_circular2
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right small-sized ungulate femora
+#'
+#' @name femur_right_circular3
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for tooth scores on right large and medium-sized ungulate femora
+#'
+#' @name femur_right_linear1
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for tooth scores on right large and medium-sized ungulate femora
+#'
+#' @name femur_right_linear2
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right large-sized ungulate humeri
+#'
+#' @name humerus_right_circular1
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right medium-sized ungulate humeri
+#'
+#' @name humerus_right_circular2
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right small-sized ungulate humeri
+#'
+#' @name humerus_right_circular3
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right large-sized ungulate metacarpal bones
+#'
+#' @name metacarpus_right_circular1
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right large-sized ungulate metatarsal bones
+#'
+#' @name metatarsus_right_circular1
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right medium-sized ungulate metatarsal bones
+#'
+#' @name metatarsus_right_circular2
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right small-sized ungulate metatarsal bones
+#'
+#' @name metatarsus_right_circular3
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right large-sized ungulate radii
+#'
+#' @name radius_right_circular1
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right medium-sized ungulate radii
+#'
+#' @name radius_right_circular2
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right small-sized ungulate radii
+#'
+#' @name radius_right_circular3
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right large-sized ungulate tibiae
+#'
+#' @name tibia_right_circular1
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right medium-sized ungulate tibiae
+#'
+#' @name tibia_right_circular2
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' x, y and z coordinates for pits on right small-sized ungulate tibiae
+#'
+#' @name tibia_right_circular3
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords datasets
+NULL
+
+#' 3D model of cervid left femur
+#'
+#' @name left_femur
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid right femur
+#'
+#' @name right_femur
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid left humerus
+#'
+#' @name left_humerus
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid right humerus
+#'
+#' @name right_humerus
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid left radius
+#'
+#' @name left_radius
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid right radius
+#'
+#' @name right_radius
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid left tibia
+#'
+#' @name left_tibia
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid right tibia
+#'
+#' @name right_tibia
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid left metacarpus
+#'
+#' @name left_metacarpus
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid right metacarpus
+#'
+#' @name right_metacarpus
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid left metatarsus
+#'
+#' @name left_metatarsus
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
+#' 3D model of cervid right metatarsus
+#'
+#' @name right_metatarsus
+#' @docType data
+#' @author Julia Aramendi
+#' @references in prep
+#' @keywords 3D model
+NULL
+
